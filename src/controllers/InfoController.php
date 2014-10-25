a77c453552cac9cb02d9e9253f97ebaa15589061
chris15001900 <krzysztofbaczkiewicz@gmail.com>
2014-10-25 21:04:33 +0200

    Many changes for Info Controller

--- src/controllers/InfoController.php
+++ src/controllers/InfoController.php
@@ -12,68 +12,70 @@ class InfoController extends Controller {
 
 	public function edit() {
 
-		\Request::$dontFlash = ['avatar'];
+		try {
 
-		if(\Input::hasFile('avatar')) {
+			if(\Input::hasFile('avatar')) {
 
-			$name = \Str::random(32) . \Str::random(32) . '.png';
-			$image = \Image::make(\Input::file('avatar')->getRealPath());
+				$name = \Str::random(32) . \Str::random(32) . '.png';
+				$image = \Image::make(\Input::file('avatar')->getRealPath());
 
-            $image->resize(300, 300)->save(public_path('assets/info/' . $name));
+	            $image->resize(300, 300)->save(public_path('assets/info/' . $name));
 
-			$object = Info::where('key', 'avatar')->first();
-
-			if($object) {
-
-				$object->value = $name;
-				$object->save();
-
-			} else {
-
-				Info::create([
-
-					'key' => 'avatar',
-					'value' => $name
-
-				]);
-
-			}
-
-		}
-
-		$fields = [ 'header', 'about', 'address' ];
-
-		foreach($fields as $field) {
-
-			if(!\Request::get($field)) continue;
-
-			if(module('Info')->$field()) {
-
-				$object = Info::where('key', $field)->first();
+				$object = Info::where('key', 'avatar')->first();
 
 				if($object) {
 
-					$object->value = \Request::get($field);
+					$object->value = $name;
 					$object->save();
+
+				} else {
+
+					Info::create([
+
+						'key' => 'avatar',
+						'value' => $name
+
+					]);
 
 				}
 
-			} else {
+			}
 
-				Info::create([
+			$fields = [ 'header', 'about', 'address' ];
 
-					'key' => $field,
-					'value' => \Request::get($field)
+			foreach($fields as $field) {
 
-				]);
+				if(!\Request::get($field)) continue;
+
+				if(module('Info')->$field()) {
+
+					$object = Info::where('key', $field)->first();
+
+					if($object) {
+
+						$object->value = \Request::get($field);
+						$object->save();
+
+					}
+
+				} else {
+
+					Info::create([
+
+						'key' => $field,
+						'value' => \Request::get($field)
+
+					]);
+
+				}
 
 			}
 
-		}
+			\Flash::success('Changes has been saved.');
 
-		\Flash::success('Changes has been saved.');
+			return \Redirect::back();
 
-		return \Redirect::back();
+		} catch(InvalidArgumentException $e) {}
 
 	}
 
